f7f10d6818b8c66cc1698d1d377bf6496050e31c
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 16:26:16 +0200

    add switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ShowProfiles from './components/ShowProfiles'
 import Header from './components/Header'
 import './styles/App.scss';
 
 function App() {
+  let [switch1, setSwitch1] = useState(false)
+  const toggleSwitch1 = () => setSwitch1(!switch1)
+
   return (
-    <div className="App">
+    <div id="App">
       <Header />
-      <ShowProfiles sorting='DISTANCE'/>
+        <div className='custom-control custom-switch'>
+          <input
+            type='checkbox'
+            className='custom-control-input'
+            id='customSwitches'
+            checked={switch1}
+            onChange={toggleSwitch1}
+            readOnly
+          />
+          <label className='custom-control-label' htmlFor='customSwitches'>Sort by activity</label>
+        </div>
+      <ShowProfiles sorting={switch1 ? 'ACTIVITY' : 'DISTANCE'}/>
     </div>
   );
 }
