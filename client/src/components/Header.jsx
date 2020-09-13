d91a9e91d1abd2599158751c8f888ae3ce1fb1ee
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 15:21:35 +0200

    add romeo logo to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import romeoLogo from '../media/logo.png'
 import '../styles/Header.scss'
 
 export default function Header() {
     return (
         <div id="header">
+            <img src={romeoLogo} alt="Romeo-Logo"/>
             <h1>Romeo Romance</h1>
         </div>
     )
