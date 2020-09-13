b79c8b60ad1f3ba45f84c61d84eff3ab64b9c556
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 16:29:55 +0200

    add entry animation to romeo logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { MDBAnimation } from 'mdbreact';
 import romeoLogo from '../media/logo.png'
 import '../styles/Header.scss'
 
 export default function Header() {
     return (
         <div id="header">
-            <img src={romeoLogo} alt="Romeo-Logo"/>
+            <MDBAnimation type="flipInX">
+                <img src={romeoLogo} alt="Romeo-Logo"/>
+            </MDBAnimation>
             <h1>Romeo Romance</h1>
         </div>
     )
