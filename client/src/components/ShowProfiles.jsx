97ce1b88113d488ff44d7e64eacfbf28b90353f8
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 16:28:27 +0200

    switch sorting on toggle switch

--- client/src/components/ShowProfiles.jsx
+++ client/src/components/ShowProfiles.jsx
@@ -8,10 +8,12 @@ export default function ShowProfiles(props) {
     let [currentProfiles, setCurrentProfiles] = useState(null);
     let [querySize, setQuerySize] = useState(10)
     let [sorting, setSorting] = useState(props.sorting);
-    
+
+    useEffect(() => setSorting(props.sorting), [props.sorting])
+
     useEffect(() => {
         api.fetchAllData(querySize, sorting).then(res => setCurrentProfiles(res))
-    }, [])
+    }, [sorting])
 
     return (
         <div id="show-profiles">
