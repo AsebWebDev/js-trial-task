664fdc317266473858c2624f24e1dbbfd58411ee
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 12:47:13 +0200

    add basic currentProfiles value

--- client/src/components/ShowProfiles.jsx
+++ client/src/components/ShowProfiles.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/ShowProfiles.scss'
 
 export default function ShowProfiles() {
+    let [currentProfiles, setCurrentProfiles] = useState(null);
+    
+    useEffect(() => {
+        
+    }, [])
+
     return (
         <div id="show-profiles">
-            
+           {currentProfiles && <p>Profiles exists</p>} 
+           {!currentProfiles && <p>Loading...</p>}
         </div>
     )
 }
