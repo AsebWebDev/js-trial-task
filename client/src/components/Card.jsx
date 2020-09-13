bbef78ff6447bf198e4ab93488d4134adfd7c021
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 13:40:45 +0200

    add react card flip

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactCardFlip from 'react-card-flip';
 import '../styles/Card.scss'
 
 export default function Card() {
+    let [isFlipped, setIsFlipped] = useState(false)
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setIsFlipped(!isFlipped)
+    }
+
     return (
         <div id="card">
-            
+            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+                {/* ###### F R O N T ###### */}
+                <div className="card-front" onClick={handleClick}>
+                        front   
+                </div>
+        
+                {/* ###### B A C K ###### */}
+                <div className="card-back" onClick={handleClick}>
+                    back
+                </div>
+            </ReactCardFlip>
         </div>
     )
 }
