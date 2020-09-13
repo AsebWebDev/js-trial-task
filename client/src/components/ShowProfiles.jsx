cf2196afd6362cb31a8f20a3f4275926081c8dd6
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-13 16:36:38 +0200

    implement react-infinite-scroll-component

--- client/src/components/ShowProfiles.jsx
+++ client/src/components/ShowProfiles.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { MDBContainer } from 'mdbreact';
 import Card from './Card'
 import Loading from './Loading';
 import api from '../api'
@@ -9,6 +11,13 @@ export default function ShowProfiles(props) {
     let [querySize, setQuerySize] = useState(10)
     let [sorting, setSorting] = useState(props.sorting);
 
+    const loadFunc = () => {
+        api.fetchAllData(querySize+10, sorting).then(res => {
+            setCurrentProfiles(res)
+            setQuerySize(querySize+10)
+        })
+    }
+
     useEffect(() => setSorting(props.sorting), [props.sorting])
 
     useEffect(() => {
@@ -17,8 +26,22 @@ export default function ShowProfiles(props) {
 
     return (
         <div id="show-profiles">
-           {currentProfiles && currentProfiles.map( (item, i) => <Card key={i} profile={item} />)}
-           {!currentProfiles && <Loading />}
+           {currentProfiles && <InfiniteScroll
+                dataLength={currentProfiles.length} //This is important field to render the next data
+                next={loadFunc}
+                hasMore={true}
+                loader={<div className="spinner-grow text-danger" role="status"><span className="sr-only" /></div>}
+                endMessage={
+                    <p style={{textAlign: 'center'}}>
+                    <b>Yay! You have seen it all</b>
+                    </p>
+                }
+            >
+                <MDBContainer id="show-profiles">
+                    {currentProfiles && currentProfiles.map( (item, i) => <Card key={i} profile={item} />)}
+                </MDBContainer>
+            </InfiniteScroll>}
+        {!currentProfiles && <Loading />}
         </div>
     )
 }
